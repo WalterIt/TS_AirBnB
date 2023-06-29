ec3dfe4798758c36a5b9e488ca390045eeb5fc82
WalterIt <whiruba@gmail.com>
2023-06-29 17:48:43 -0300

    Listing creation step 2 (Location selection, Map component, Country autocomplete)

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,8 +19,10 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`relative disabled:opacity-70 disabled:cursor-not-allowed tracking-widest rounded-lg hover:bg-rose-500 hover:scale-[99%] hover:text-white transition w-full ${
-        outline ? "bg-white border-black text-black " : "bg-rose-500 text-white"
+      className={`relative disabled:opacity-70 disabled:cursor-not-allowed tracking-widest rounded-lg hover:bg-rose-500 hover:scale-[99%] hover:border-rose-600 hover:text-white transition w-full ${
+        outline
+          ? "bg-white border-black text-black "
+          : "bg-rose-500  text-white"
       } ${
         small
           ? "py-1 text-sm font-light border "
