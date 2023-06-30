22ac13b84eb4084752ac14027c2faafe19fcf861
WalterIt <whiruba@gmail.com>
2023-06-30 17:04:19 -0300

    Fetching listings with server components (Listing card component, direct server action)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import getCurrentUser from "@/actions/getCurrentUser";
+import getListings from "@/actions/getListings";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
+import ListingCard from "@/components/listings/ListingCard";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+  const isEmpty = true;
+
+  if (!listings.length) {
+    return <EmptyState showReset />;
+  }
   return (
-    <div className="text-rose-500 text-3xl">
-      <h1>Hello Airbnb!</h1>
-    </div>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            currentUser={currentUser}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
   );
 }
